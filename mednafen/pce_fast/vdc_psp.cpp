4b30ee7b650a1711468ed47634d90eaa4b7d8bff
aliaspider <aliaspider@gmail.com>
2014-11-05 15:51:18 +0100

    (psp) hw_render: can display sprite tilemap.

--- mednafen/pce_fast/vdc_psp.cpp
+++ mednafen/pce_fast/vdc_psp.cpp
@@ -35,7 +35,132 @@ void init_video_ge(void)
 {
 
 }
-void fix_tile_cache_ge(uint16 A)
+
+void fix_tile_cache_sprites(uint16 A)
+{
+   u64* line_ge = ((u64*)PCE_VRAMTEXTURE_SPRITE) + ((A >> 6) << 4) +
+                  ((A & 0x7) << 1) + ((A >> 3) & 0x1);
+
+   union
+   {
+      struct __attribute((packed))
+      {
+         unsigned p15: 1, p14: 1, p13: 1, p12: 1, p11: 1, p10: 1, p9 : 1, p8 : 1;
+         unsigned p7 : 1, p6 : 1, p5 : 1, p4 : 1, p3 : 1, p2 : 1, p1 : 1, p0 : 1;
+      };
+      u16 val;
+   } src_line;
+
+   union
+   {
+      struct __attribute((packed))
+      {
+         unsigned p0_b0: 1, p0_b1: 1, p0_b2: 1, p0_b3: 1;
+         unsigned p1_b0: 1, p1_b1: 1, p1_b2: 1, p1_b3: 1;
+         unsigned p2_b0: 1, p2_b1: 1, p2_b2: 1, p2_b3: 1;
+         unsigned p3_b0: 1, p3_b1: 1, p3_b2: 1, p3_b3: 1;
+         unsigned p4_b0: 1, p4_b1: 1, p4_b2: 1, p4_b3: 1;
+         unsigned p5_b0: 1, p5_b1: 1, p5_b2: 1, p5_b3: 1;
+         unsigned p6_b0: 1, p6_b1: 1, p6_b2: 1, p6_b3: 1;
+         unsigned p7_b0: 1, p7_b1: 1, p7_b2: 1, p7_b3: 1;
+         unsigned p8_b0: 1, p8_b1: 1, p8_b2: 1, p8_b3: 1;
+         unsigned p9_b0: 1, p9_b1: 1, p9_b2: 1, p9_b3: 1;
+         unsigned p10_b0: 1, p10_b1: 1, p10_b2: 1, p10_b3: 1;
+         unsigned p11_b0: 1, p11_b1: 1, p11_b2: 1, p11_b3: 1;
+         unsigned p12_b0: 1, p12_b1: 1, p12_b2: 1, p12_b3: 1;
+         unsigned p13_b0: 1, p13_b1: 1, p13_b2: 1, p13_b3: 1;
+         unsigned p14_b0: 1, p14_b1: 1, p14_b2: 1, p14_b3: 1;
+         unsigned p15_b0: 1, p15_b1: 1, p15_b2: 1, p15_b3: 1;
+      };
+
+      u64 val;
+   } dst_line;
+
+   src_line.val = vdc->VRAM[A];
+   dst_line.val = *line_ge;
+
+   switch ((A>>4)&0x3)
+   {
+   case 0:
+      dst_line.p0_b0 = src_line.p0;
+      dst_line.p1_b0 = src_line.p1;
+      dst_line.p2_b0 = src_line.p2;
+      dst_line.p3_b0 = src_line.p3;
+      dst_line.p4_b0 = src_line.p4;
+      dst_line.p5_b0 = src_line.p5;
+      dst_line.p6_b0 = src_line.p6;
+      dst_line.p7_b0 = src_line.p7;
+      dst_line.p8_b0 = src_line.p8;
+      dst_line.p9_b0 = src_line.p9;
+      dst_line.p10_b0 = src_line.p10;
+      dst_line.p11_b0 = src_line.p11;
+      dst_line.p12_b0 = src_line.p12;
+      dst_line.p13_b0 = src_line.p13;
+      dst_line.p14_b0 = src_line.p14;
+      dst_line.p15_b0 = src_line.p15;
+      break;
+   case 1:
+      dst_line.p0_b1 = src_line.p0;
+      dst_line.p1_b1 = src_line.p1;
+      dst_line.p2_b1 = src_line.p2;
+      dst_line.p3_b1 = src_line.p3;
+      dst_line.p4_b1 = src_line.p4;
+      dst_line.p5_b1 = src_line.p5;
+      dst_line.p6_b1 = src_line.p6;
+      dst_line.p7_b1 = src_line.p7;
+      dst_line.p8_b1 = src_line.p8;
+      dst_line.p9_b1 = src_line.p9;
+      dst_line.p10_b1 = src_line.p10;
+      dst_line.p11_b1 = src_line.p11;
+      dst_line.p12_b1 = src_line.p12;
+      dst_line.p13_b1 = src_line.p13;
+      dst_line.p14_b1 = src_line.p14;
+      dst_line.p15_b1 = src_line.p15;
+      break;
+   case 2:
+      dst_line.p0_b2 = src_line.p0;
+      dst_line.p1_b2 = src_line.p1;
+      dst_line.p2_b2 = src_line.p2;
+      dst_line.p3_b2 = src_line.p3;
+      dst_line.p4_b2 = src_line.p4;
+      dst_line.p5_b2 = src_line.p5;
+      dst_line.p6_b2 = src_line.p6;
+      dst_line.p7_b2 = src_line.p7;
+      dst_line.p8_b2 = src_line.p8;
+      dst_line.p9_b2 = src_line.p9;
+      dst_line.p10_b2 = src_line.p10;
+      dst_line.p11_b2 = src_line.p11;
+      dst_line.p12_b2 = src_line.p12;
+      dst_line.p13_b2 = src_line.p13;
+      dst_line.p14_b2 = src_line.p14;
+      dst_line.p15_b2 = src_line.p15;
+      break;
+   case 3:
+   default:
+      dst_line.p0_b3 = src_line.p0;
+      dst_line.p1_b3 = src_line.p1;
+      dst_line.p2_b3 = src_line.p2;
+      dst_line.p3_b3 = src_line.p3;
+      dst_line.p4_b3 = src_line.p4;
+      dst_line.p5_b3 = src_line.p5;
+      dst_line.p6_b3 = src_line.p6;
+      dst_line.p7_b3 = src_line.p7;
+      dst_line.p8_b3 = src_line.p8;
+      dst_line.p9_b3 = src_line.p9;
+      dst_line.p10_b3 = src_line.p10;
+      dst_line.p11_b3 = src_line.p11;
+      dst_line.p12_b3 = src_line.p12;
+      dst_line.p13_b3 = src_line.p13;
+      dst_line.p14_b3 = src_line.p14;
+      dst_line.p15_b3 = src_line.p15;
+      break;
+
+   }
+
+   *line_ge = dst_line.val;
+}
+
+void fix_tile_cache_bg(uint16 A)
 {
    u32* line_ge = ((u32*)PCE_VRAMTEXTURE_BG) + ((A >> 6) << 5) +
                   ((A & 0x7) << 2) + ((A >> 4) & 0x3);
@@ -43,13 +168,15 @@ void fix_tile_cache_ge(uint16 A)
    {
       struct __attribute((packed))
       {
-         unsigned p7_b0: 1, p6_b0: 1, p5_b0: 1, p4_b0: 1, p3_b0: 1, p2_b0: 1, p1_b0: 1, p0_b0: 1;
-         unsigned p7_b1: 1, p6_b1: 1, p5_b1: 1, p4_b1: 1, p3_b1: 1, p2_b1: 1, p1_b1: 1, p0_b1: 1;
+         unsigned p7_b0: 1, p6_b0: 1, p5_b0: 1, p4_b0: 1, p3_b0: 1, p2_b0: 1, p1_b0: 1,
+                  p0_b0: 1;
+         unsigned p7_b1: 1, p6_b1: 1, p5_b1: 1, p4_b1: 1, p3_b1: 1, p2_b1: 1, p1_b1: 1,
+                  p0_b1: 1;
 
 
       };
       u16 val;
-   }src_line;
+   } src_line;
    union
    {
       struct __attribute((packed))
@@ -64,7 +191,7 @@ void fix_tile_cache_ge(uint16 A)
          unsigned p7_b0: 1, p7_b1: 1, p7_b2: 1, p7_b3: 1;
       };
       u32 val;
-   }dst_line;
+   } dst_line;
 
    src_line.val = vdc->VRAM[A];
    dst_line.val = *line_ge;
@@ -112,13 +239,21 @@ void fix_tile_cache_ge(uint16 A)
    }
 
    *line_ge = dst_line.val;
+
+}
+
+void fix_tile_cache_ge(uint16 A)
+{
+   fix_tile_cache_bg(A);
+   fix_tile_cache_sprites(A);
 }
 
 void pce_draw_tilemap(void)
 {
 
    sceGuTexFunc(GU_TFX_REPLACE, GU_TCC_RGB);
-   sceGuTexImage(0, 256, 256, 256, PCE_VRAMTEXTURE_BG);
+//   sceGuTexImage(0, 256, 256, 256, PCE_VRAMTEXTURE_BG);
+   sceGuTexImage(0, 256, 256, 256, PCE_VRAMTEXTURE_SPRITE+16*1024);
    //   sceGuTexImage(0, 256, 256, 256, vdc->bg_tile_cache[0]);
 
    sceGuTexMode(GU_PSM_T4, 0, 0, GU_TRUE);
