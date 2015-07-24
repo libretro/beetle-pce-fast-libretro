a1ad863aac635c9c6ae712b0adb2547a08c6336d
twinaphex <libretro@gmail.com>
2015-07-25 01:18:05 +0200

    Cleanups

--- mednafen/math_ops.h
+++ mednafen/math_ops.h
@@ -18,25 +18,6 @@ static INLINE uint32 round_up_pow2(uint32 v)
    return(v);
 }
 
-static INLINE uint32 uilog2(uint32 v)
-{
- // http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
-
- static const uint32 MultiplyDeBruijnBitPosition[32] = 
- {
-   0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
-   8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
- };
-
- v |= v >> 1; // first round down to one less than a power of 2 
- v |= v >> 2;
- v |= v >> 4;
- v |= v >> 8;
- v |= v >> 16;
-
- return MultiplyDeBruijnBitPosition[(uint32_t)(v * 0x07C4ACDDU) >> 27];
-}
-
 // Some compilers' optimizers and some platforms might fubar the generated code from these macros,
 // so some tests are run in...tests.cpp
 #define sign_8_to_s16(_value) ((int16)(int8)(_value))
