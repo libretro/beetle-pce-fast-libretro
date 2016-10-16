df09cbdfbfbe83d051a3f54cdb971d64bc326323
Tatsuya79 <jeanpascalmeunier@hotmail.com>
2016-10-16 15:06:16 +0200

    No sprite limit needs restart too

--- libretro.cpp
+++ libretro.cpp
@@ -1732,7 +1732,7 @@ void retro_set_environment(retro_environment_t cb)
 
    static const struct retro_variable vars[] = {
       { "pce_fast_cdimagecache", "CD Image Cache (Restart); disabled|enabled" },
-      { "pce_nospritelimit", "No Sprite Limit; disabled|enabled" },
+      { "pce_nospritelimit", "No Sprite Limit (Restart); disabled|enabled" },
       { "pce_hoverscan", "Horizontal Overscan (Restart); disabled|enabled" },
       { "pce_initial_scanline", "Initial scanline; 3|4|5|6|7|8|9|10|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|0|1|2" },
       { "pce_last_scanline", "Last scanline; 242|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241" },
