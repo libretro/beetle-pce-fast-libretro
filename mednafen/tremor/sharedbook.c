1c693c630121366f0b819d785762f1ce5b3a68d0
Craig <1188869+cscd98@users.noreply.github.com>
2026-09-25 17:40:10 +0100

    libretro: second point as int32_t to match function call (#248)

--- mednafen/tremor/sharedbook.c
+++ mednafen/tremor/sharedbook.c
@@ -263,7 +263,7 @@ int32_t *_book_unquantize(const static_codebook *b,int n,int *sparsemap,
                   int         lastpoint=0;
 
                   for(k=0;k<b->dim;k++){
-                     int point=0;
+                     int32_t point=0;
                      int val=VFLOAT_MULTI(delta,delpoint,
                            abs(b->quantlist[j*b->dim+k]),&point);
 
